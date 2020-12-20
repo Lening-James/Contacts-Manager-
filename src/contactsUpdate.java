1242c0827b287f634526f4444aecc672d6b13df3
James Gemes <james.gemes@gmail.com>
2020-12-19 22:31:48 -0600

    working on the update class

--- src/contactsUpdate.java
+++ src/contactsUpdate.java
@@ -1,2 +1,15 @@
-public class contactsUpdate {
-}
+package src;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class ContactsUpdate {
+    public static List<Contacts> Personlist = new ArrayList<>();
